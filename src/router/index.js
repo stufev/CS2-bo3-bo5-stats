f505392582292e5555bbd94621bf83de50cd82a8
Roman Filippov <filippov.r@vipavenue.ru>
2024-05-04 19:33:13 +0300

    add player page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from "../views/Home/index.vue"
+import Player from "../views/Player/index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,12 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/player/:name',
+      name: 'player',
+      component: Player,
+      props: true,
     },
   ]
 })
