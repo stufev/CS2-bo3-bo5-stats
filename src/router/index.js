9a0dc55db0a80d6c6cd1f044056a679b443a5bc5
Roman Filippov <filippov.r@vipavenue.ru>
2024-06-02 20:37:24 +0300

    add create game line-up page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Home from "../views/Home/index.vue"
 import Player from "../views/Player/index.vue";
 import Login from "../views/Login/index.vue";
 import Signup from "../views/Signup/index.vue";
+import CreateGame from "../views/CreateGame/index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,6 +28,11 @@ const router = createRouter({
       path: '/signup',
       name: 'Signup',
       component: Signup,
+    },
+    {
+      path: '/create-game',
+      name: 'CreateGame',
+      component: CreateGame,
     },
   ]
 })
